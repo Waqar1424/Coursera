200c82dcff85c21b30b3137731b330691db11747
Waqar Ahmed <waqarahmedr66@gmail.com>
2022-02-01 11:38:24 +0500

    Back to Assignment#1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import react, {Component} from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-import {DISHES} from './shared/dishes'
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
 
-    constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES,
+        
+    };
+  }
 
-      this.state ={
-        dishes: DISHES
-      };
-    }
+
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-          <Menu dishes={this.state.dishes} />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
