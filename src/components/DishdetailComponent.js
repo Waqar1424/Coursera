775976edf2d4b1fd3da39cdf6a395be87195f9d9
Waqar Ahmed <waqarahmedr66@gmail.com>
2022-03-04 17:27:47 +0500

    Fetch From Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import {Card,CardImg,CardTitle,CardBody,CardText,Breadcrumb,BreadcrumbItem,Button,Modal, ModalHeader, ModalBody,Label, Row, Col} from "reactstrap";
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const required =(val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -102,8 +102,8 @@ handleSubmit(values){
   function RenderDish({dish}) {
         return (
       <Card>
-        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-        <CardBody>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
         </CardBody>
